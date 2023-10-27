c0d6f20cf439525c9b5ee4fea8f2c222215117dd
Diego Alleyne <714289@student.dusd.net>
2023-10-27 11:39:15 -0700

    Syntax
    Created a syntax code to a game object.

---
+++ ScriptingProjectP3Alleyne/Assets/Scripts/Syntax.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Syntax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
+    }
+}
