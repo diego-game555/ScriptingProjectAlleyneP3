52ec73771cf1d4572fae667fe1d13182ee677723
Diego Alleyne <714289@student.dusd.net>
2024-02-07 12:15:17 -0800

    Linear Interpolation
    Created script for linear interpolation.

---
+++ ScriptingProjectP3Alleyne/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    // Start is called before the first frame update
+    void Start()
+    {
+        // In this case, result = 4
+        float result = Mathf.Lerp(3f, 5f, 0.5f);
+
+        Vector3 from = new Vector3(1f, 2f, 3f);
+        Vector3 to = new Vector3(5f, 6f, 7f);
+
+        // Here result = (4, 5, 6)
+        Vector3 result = Vector3.Lerp(from, to, 0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Intensity of the light will start off at 0 and will be set to 4 after the first update,then 6, 7, and 7.5
+        Light.intensity = Mathf.Lerp(Light.intensity, 8f, 0.5f);
+    
+    }
+    */
+}
