548a34ef4177ce1c1657afbc213435555b7a69b7
Diego Alleyne <714289@student.dusd.net>
2025-02-04 11:45:53 -0800

    Creating Properties
    Finished creating properties 2/4

---
+++ ScriptingProjectP3Alleyne/Assets/Scripts/CreatingProperties/Game.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+  
+    void Start()
+    {
+        Player myPlayer = new Player();
+
+        myPlayer.Experience = 5;
+        int x = myPlayer.Experience;
+    }
+
+}
