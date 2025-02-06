be29528ab8d66631dfb9b20e2b021385530efd59
Diego Alleyne <714289@student.dusd.net>
2025-02-06 10:47:25 -0800

    Completed Statics
    Completed Statics script 2/6

---
+++ ScriptingProjectP3Alleyne/Assets/Scripts/Statics/Enemy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy
+{
+    public static int enemyCount = 0;
+
+    public Enemy()
+    {
+        enemyCount++;
+    }
+}
---
+++ ScriptingProjectP3Alleyne/Assets/Scripts/Statics/UtilitiesExample.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilitiesExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = Utilities.Add(5, 6);
+    }
+}
