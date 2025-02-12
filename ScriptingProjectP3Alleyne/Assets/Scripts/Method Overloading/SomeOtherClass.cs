425723a1ca47534cf66c64e4c957ef4d3a46434e
Diego Alleyne <714289@student.dusd.net>
2025-02-12 11:45:31 -0800

    Method Overloading
    Did code for method overloading 2/12

---
+++ ScriptingProjectP3Alleyne/Assets/Scripts/Method Overloading/SomeOtherClass.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeOtherClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SomeClass myClass = new SomeClass();
+
+        myClass.Add(1, 2);
+        myClass.Add("Hello ", "World");
+    }
+}
